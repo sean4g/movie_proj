e08fc74f7f7026dfc9337bd89a1ceb960dbaafae
sean4g <wang4615@purdue.edu>
2024-07-08 16:43:04 -0700

    reformat

---
+++ backend/gradle/wrapper/src/test/java/com/example/MovieWatcher/MovieWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieWatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
